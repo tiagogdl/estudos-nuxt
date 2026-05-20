1a952424042bf856ddc627ce4a0d634ed32910ca
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-05-20 19:49:25 -0300

    Projeto iniciado

--- projeto-rick-and-morty/nuxt-app/app/app.vue
+++ projeto-rick-and-morty/nuxt-app/app/app.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-purple-600">Código ao Ponto</h1>
+
+    <div class="flex w-full-max-w-[1224px] gap-4 mx-auto">
+      <div v-for="currentCharacter in data.results">
+          <img :src="currentCharacter.image" alt="Imagens personagens">
+          <p>{{ currentCharacter.name }}</p>
+          <p>{{ currentCharacter.status }}</p>
+          <p>{{ currentCharacter.species }}</p>
+          <p>{{ currentCharacter.origin.name}}</p>
+
+          
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const { data, status, error, refresh, clear } = await useFetch('https://rickandmortyapi.com/api/character')
+
+console.log(data.value)
+
+</script>
