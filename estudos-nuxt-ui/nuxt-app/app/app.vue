9c4440aa6a4f91709d31d1e80c61b7ecc15aacad
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-05-24 11:53:42 -0300

    Visto propriedades básicas do Nuxt Ui

---
+++ estudos-nuxt-ui/nuxt-app/app/app.vue
@@ -0,0 +1,50 @@
+<template>
+  <UApp>
+
+    
+    <UNavigationMenu :items="items">
+
+    </UNavigationMenu>
+    
+ 
+  </UApp>
+</template>
+
+<script setup>
+import { navigationMenu } from '#build/ui';
+
+const items = [
+    {
+    label: 'Guide',
+    icon: 'i-lucide-book-open',
+    to: '/docs/getting-started',
+    },
+      {
+        label: 'Introduction',
+        description: 'Fully styled and customizable components for Nuxt.',
+        icon: 'i-lucide-house'
+      },
+      {
+        label: 'Installation',
+        description: 'Learn how to install and configure Nuxt UI in your application.',
+        icon: 'i-lucide-cloud-download'
+      },
+      {
+        label: 'Icons',
+        icon: 'i-lucide-smile',
+        description: 'You have nothing to do, @nuxt/icon will handle it automatically.'
+      },
+      {
+        label: 'GitHub',
+        icon: 'i-simple-icons-github',
+        badge: '6k',
+        to: 'https://github.com/nuxt/ui',
+        target: '_blank'
+      },
+      {
+        label: 'TesteTiago',
+        icon: 'i-lucide-circle-help'
+        
+      }
+]
+</script>
---
+++ estudos-nuxt-ui/nuxt-app/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
